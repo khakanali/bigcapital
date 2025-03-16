5a800059a8f7e19418d21851d735ca5cfbbfcf53
khakanali <khakan.ali@alpharages.com>
2025-03-16 23:43:59 +0500

    update vercel settings

--- packages/server/src/before.ts
+++ packages/server/src/before.ts
@@ -1,12 +1,19 @@
 import path from 'path';
 import moment from 'moment';
+import { fileURLToPath } from 'url';
 
+// Replace __dirname with ES Module-compatible equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Assign paths to global
 global.__root_dir = path.join(__dirname, '..');
 global.__resources_dir = path.join(global.__root_dir, 'resources');
 global.__locales_dir = path.join(global.__resources_dir, 'locales');
 global.__views_dir = path.join(global.__root_dir, 'views');
 global.__storage_dir = path.join(global.__root_dir, 'storage');
 
+// Moment prototype extension
 moment.prototype.toMySqlDateTime = function () {
   return this.format('YYYY-MM-DD HH:mm:ss');
 };
---
+++ packages/server/src/global.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface Global {
+    __root_dir: string;
+    __resources_dir: string;
+    __locales_dir: string;
+    __views_dir: string;
+    __storage_dir: string;
+  }
+}
